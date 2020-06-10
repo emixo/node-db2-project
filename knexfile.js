c50fe3b39cd28f84f5096c7405de4c90e1912f73
emilio <ediaz-goico@me.bergen.edu>
2020-06-10 19:20:06 -0400

    okay but for real this time i finished mvp, i created my migrations again because i deleted it by accident

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,9 @@ module.exports = {
         filename: './data/car-dealer.db3',
       },
       useNullAsDefault: true,
+      migrations: {
+          directory: "./data/migrations"
+      }
     },
   
     staging: {
